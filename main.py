14986122e430b606770cfd85efafc1abde40e620
CurasaoRG <rashit.r.gafarov@gmail.com>
2025-08-01 15:00:52 +0300

    fix:main added mailbox

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ config = dotenv_values(ROOT / ".env")
 repo = CsvRepository(ROOT / "data")
 inc = IncrementHandler(ROOT / "data" / "increment.txt")
 
-with ImapClient(config["GMAIL_USERNAME"], config["GMAIL_PASSWORD"]) as client:
+with ImapClient(config["GMAIL_USERNAME"], config["GMAIL_PASSWORD"], config["MAILBOX"]) as client:
     for raw_msg in client.fetch_new_raw(since_uid=inc.get()):
         check = parse_message(raw_msg.raw_bytes, raw_msg.mail_from)
         repo.append_check(check_id=int(raw_msg.uid), check=check)

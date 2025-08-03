149eb9514e2aaf973bedef3f416bf7beb4a6ede4
CurasaoRG <rashit.r.gafarov@gmail.com>
2025-08-03 15:33:16 +0300

    feature:imap_client added next uid search

--- vkusvill/imap_client.py
+++ vkusvill/imap_client.py
@@ -4,7 +4,8 @@ import imaplib
 import email
 from dataclasses import dataclass
 from typing import Iterator, List
-
+import re
+import bisect
 
 @dataclass
 class RawMessage:
@@ -28,7 +29,7 @@ class ImapClient:
         self.mailbox = mailbox
         self._imap: imaplib.IMAP4_SSL | None = None
         self._allowed_cache: set[int] | None = None   # кэш UID-ов
-        imaplib.Debug = 4 # debug option, shows mailbox requests
+        # imaplib.Debug = 4 # debug option, shows mailbox requests
 
     def _ensure_cache(self) -> set[int]:
         if self._allowed_cache is not None:
@@ -38,14 +39,21 @@ class ImapClient:
         typ, data = self._imap.uid('SEARCH', 'CHARSET UTF-8', 'OR (FROM "noreply-cloudkassir@cp.ru") SUBJECT')
         if typ != "OK":
             raise RuntimeError("IMAP search failed")
-        self._allowed_cache  = set(map(int, data[0].decode().split()))
+        self._allowed_cache  = sorted(set(map(int, data[0].decode().split())))
         return self._allowed_cache
 
     def refresh_cache(self) -> None:
         self._allowed_cache = None
-        
+
+    def _find_next_uid(self, N):
+        # allowed = self._ensure_cache()
+        index = bisect.bisect_right(self._allowed_cache, N)
+        if index < len(self._allowed_cache):
+            return index
+        return None
+
     # ---------- context-manager ----------
-    def __enter__(self) -> "ImapClient":
+    def __enter__(self) -> ImapClient:
         self._imap = imaplib.IMAP4_SSL("imap.gmail.com")
         self._imap.login(self.username, self.password)
         self._imap.select(self.mailbox)
@@ -64,7 +72,8 @@ class ImapClient:
     def fetch_new_raw(self, since_uid: int = 0) -> Iterator[RawMessage]:
         allowed = self._ensure_cache()
         msg_body = None
-        for uid in sorted(allowed):
+        index = self._find_next_uid(since_uid)
+        for uid in sorted(allowed[index:]):
             if uid <= since_uid:
                 continue
             typ, msg_data = self._imap.uid("FETCH", str(uid), "(RFC822)")

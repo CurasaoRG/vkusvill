15c49aabac1dbdb65dcb49b7531ee9b48a72f9ca
CurasaoRG <rashit.r.gafarov@gmail.com>
2026-07-08 22:16:12 +0300

    tests fix

--- tests/models_test.py
+++ tests/models_test.py
@@ -1,7 +1,7 @@
 import unittest
 from datetime import datetime
 from decimal import Decimal
-from ..vkusvill.models import Item, Check
+from vkusvill.models import Item, Check
 
 
 class TestItem(unittest.TestCase):
